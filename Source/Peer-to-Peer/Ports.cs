add6e9a322630cc2ad972b55a87b7eccad947a30
Greg Beaty <greg.beaty2@gmail.com>
2012-11-17 15:04:52 -0500

    Added the Ports constants utility class.

---
+++ Source/Peer-to-Peer/Ports.cs
@@ -0,0 +1,9 @@
+﻿namespace ClientStream
+{
+    internal static class Ports
+    {
+        public const int RouterDiscovery = 8808;
+        public const int ServerPort = 8809;
+        public const int ClientPort = 8810;
+    }
+}
